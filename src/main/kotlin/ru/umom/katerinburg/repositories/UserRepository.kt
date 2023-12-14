f625aeab5e0d55fe3d929292c68be27a72075cf9
Ge0rg25 <Ge0rgiusGus@yandex.ru>
2023-12-14 22:38:55 +0500

    cleared the project for normal development

--- src/main/kotlin/ru/umom/katerinburg/repositories/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package ru.umom.katerinburg.repositories
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import ru.umom.katerinburg.models.UserEntity
-
-@Repository
-interface UserRepository : JpaRepository<UserEntity, String>
